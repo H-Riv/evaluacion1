404f16ef39a93a57c6b35696a569c6828f5cb8d1
HRiveros <hugo.riveros05@inacapmail.cl>
2024-07-19 11:23:39 -0400

    primer commit del index

--- mi-proyecto-nuevo/src/index.js
+++ mi-proyecto-nuevo/src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hola papusd</h1>
+    <h1>Hola papus</h1>
   </React.StrictMode>
 );
