85c4b6ca2b358061871681707527de677dde1ea3
HRiveros <hugo.riveros05@inacapmail.cl>
2024-07-19 02:53:21 -0400

    Testeando

--- mi-proyecto-nuevo/src/index.js
+++ mi-proyecto-nuevo/src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello world</h1>
+    <h1>Hola papusd</h1>
   </React.StrictMode>
 );
